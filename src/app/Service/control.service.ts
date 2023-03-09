bf9aacd6093a0b41e5131d2e621b6df1176810bd
Shankara21 <lazuardit21@gmail.com>
2023-03-09 14:27:23 +0700

    Update port

--- src/app/Service/control.service.ts
+++ src/app/Service/control.service.ts
@@ -7,7 +7,7 @@ import { catchError, Observable, throwError } from 'rxjs';
 })
 export class ControlService {
   private url = 'http://localhost:3000/';
-  // private production = 'http://192.168.9.47:3737/'
+  private production = 'http://192.168.9.47:3125/'
   constructor(private HttpClient: HttpClient) { }
   httpOptions = {
     headers: new HttpHeaders({
@@ -23,79 +23,79 @@ export class ControlService {
 
   // Categories
   getCategories() {
-    return this.HttpClient.get(this.url + 'categories')
+    return this.HttpClient.get(this.production + 'categories')
       .pipe(catchError(this.errorHttpHandler))
   }
   showCategories(id: any) {
-    return this.HttpClient.get(this.url + 'categories/' + id)
+    return this.HttpClient.get(this.production + 'categories/' + id)
       .pipe(catchError(this.errorHttpHandler))
   }
   createCategory(params: any) {
-    return this.HttpClient.post(this.url + `categories`, params)
+    return this.HttpClient.post(this.production + `categories`, params)
       .pipe(catchError(this.errorHttpHandler))
   }
   updateCategory(id: any, params: any) {
-    return this.HttpClient.put(this.url + `categories/${id}`, params)
+    return this.HttpClient.put(this.production + `categories/${id}`, params)
       .pipe(catchError(this.errorHttpHandler))
   }
   deleteCategory(id: any) {
-    return this.HttpClient.delete(this.url + `categories/${id}`)
+    return this.HttpClient.delete(this.production + `categories/${id}`)
       .pipe(catchError(this.errorHttpHandler))
   }
 
   // Articles
   getArticles() {
-    return this.HttpClient.get(this.url + 'articles')
+    return this.HttpClient.get(this.production + 'articles')
       .pipe(catchError(this.errorHttpHandler))
   }
   showArticles(title: any) {
-    return this.HttpClient.get(this.url + 'articles/' + title)
+    return this.HttpClient.get(this.production + 'articles/' + title)
       .pipe(catchError(this.errorHttpHandler))
   }
   createArticle(params: any) {
-    return this.HttpClient.post(this.url + `articles`, params)
+    return this.HttpClient.post(this.production + `articles`, params)
       .pipe(catchError(this.errorHttpHandler))
   }
   updateArticle(id: any, params: any) {
-    return this.HttpClient.put(this.url + `articles/${id}`, params)
+    return this.HttpClient.put(this.production + `articles/${id}`, params)
       .pipe(catchError(this.errorHttpHandler))
   }
   deleteArticle(id: any) {
-    return this.HttpClient.delete(this.url + `articles/${id}`)
+    return this.HttpClient.delete(this.production + `articles/${id}`)
       .pipe(catchError(this.errorHttpHandler))
   }
 
   // Auth
   login(params: any) {
-    return this.HttpClient.post(this.url + 'users/login', params)
+    return this.HttpClient.post(this.production + 'users/login', params)
   }
   register(params: any) {
-    return this.HttpClient.post(this.url + 'users/register', params)
+    return this.HttpClient.post(this.production + 'users/register', params)
   }
   logout(params: any) {
-    return this.HttpClient.delete(this.url + `users/logout/${params}`)
+    return this.HttpClient.delete(this.production + `users/logout/${params}`)
       .pipe(catchError(this.errorHttpHandler))
   }
   refreshToken(params: any) {
-    return this.HttpClient.post(this.url + `users/refreshToken`, params)
+    return this.HttpClient.post(this.production + `users/refreshToken`, params)
       .pipe(catchError(this.errorHttpHandler))
   }
 
   // User
   getUser() {
-    return this.HttpClient.get(this.url + 'users')
+    return this.HttpClient.get(this.production + 'users')
       .pipe(catchError(this.errorHttpHandler))
   }
   findUser(id: any) {
-    return this.HttpClient.get(this.url + `users/${id}`)
+    return this.HttpClient.get(this.production + `users/${id}`)
       .pipe(catchError(this.errorHttpHandler))
   }
   deleteUser(id: any) {
-    return this.HttpClient.delete(this.url + `users/${id}`)
+    return this.HttpClient.delete(this.production + `users/${id}`)
       .pipe(catchError(this.errorHttpHandler))
   }
   updateUser(id: any, params: any) {
-    return this.HttpClient.put(this.url + `users/${id}`, params)
+    return this.HttpClient.put(this.production + `users/${id}`, params)
       .pipe(catchError(this.errorHttpHandler))
   }
 
